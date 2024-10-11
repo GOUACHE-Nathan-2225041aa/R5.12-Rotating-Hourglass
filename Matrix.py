262e67287dd2fed79aa3481f2cb373e61ea81c9e
nathanGouacheg22017449 <nathan.gouache@etu.univ-amu.fr>
2024-10-11 15:23:03 +0200

    Quite good particle drop simulation final output need to be flatter

--- Matrix.py
+++ Matrix.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Matrix:
     def __init__(self, matrix_content=[]):
         self.matrix_content = matrix_content
@@ -10,28 +13,43 @@ class Matrix:
             printable_matrix += "\n"
         return printable_matrix
 
-# @todo -move the points (not only print the movement)
-# @todo -use recurrence to move the points (i the point above me can move, then ill move too) (def check_if_i_can_move)
+    def move_point(self, x, y, where):
+        if where == "down":
+            temp = self.matrix_content[y][x - 1]
+            self.matrix_content[y][x - 1] = self.matrix_content[y - 1][x - 1]
+            self.matrix_content[y - 1][x - 1] = temp
+        elif where == "down-left":
+            temp = self.matrix_content[y][x - 2]
+            self.matrix_content[y][x - 2] = self.matrix_content[y - 1][x - 1]
+            self.matrix_content[y - 1][x - 1] = temp
+        elif where == "down-right":
+            temp = self.matrix_content[y][x]
+            self.matrix_content[y][x] = self.matrix_content[y - 1][x - 1]
+            self.matrix_content[y - 1][x - 1] = temp
+
+    # @todo -delete the temp values
+    #       -make the final result completely flat
     def update_point(self, x, y):
-        print(x, y)
-        if y >= len(self.matrix_content)-1:
-            print("down already")
-        elif self.matrix_content[y + 1][x - 1] != 1:
-            print("mid")
-        elif self.matrix_content[y + 1][x - 2] != 1 and self.matrix_content[y + 1][x] != 1:
-            print("both")
-        elif x > 0 and self.matrix_content[y + 1][x - 2] != 1:
-            print("left")
-        elif x < len(self.matrix_content[y + 1])-1 and self.matrix_content[y + 1][x] != 1:
-            print("right")
-        else:
-            print("can't move")
-#
+        if self.matrix_content[y][x - 1] != 0:
+            # The point drops by 1
+            self.move_point(x, y, "down")
+        elif self.matrix_content[y][x - 2] != 0 and self.matrix_content[y][x] != 0:
+            if bool(random.getrandbits(1)):
+                self.move_point(x, y, "down-right")
+            else:
+                self.move_point(x, y, "down-left")
+        elif x > 0 and self.matrix_content[y][x - 2] != 0:
+            # The point drops by 1 on the left
+            self.move_point(x, y, "down-left")
+            return True
+        elif x < len(self.matrix_content[y - 1]) - 1 and self.matrix_content[y][x] != 0:
+            # The point drops by 1 on the right
+            self.move_point(x, y, "down-right")
+
     def update_matrix(self):
         # We check if every point can be updated
         for y in range(len(self.matrix_content)).__reversed__():
             for x in range(len(self.matrix_content[y - 1])).__reversed__():
                 # If the point is considered filled with something, we update its position
-                match self.matrix_content[y - 1][x - 1]:
-                    case 1:
-                        self.update_point(x, y)
+                if self.matrix_content[y - 1][x - 1] == 0 and not (y >= len(self.matrix_content) - 1):
+                    self.update_point(x, y)
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import random
+import time
 
 from Matrix import *
 
-matrix_size = 13
-percentage_of_spawn = 0.1
+matrix_size = 50
+percentage_of_spawn = 0.3
 
 matrix = []
 
@@ -11,11 +12,22 @@ for i in range(matrix_size):
     matrix.append([])
     for j in range(matrix_size):
         if random.random() < percentage_of_spawn:
-            matrix[i].append(1)
-        else:
             matrix[i].append(0)
+        else:
+            matrix[i].append(" ")
 
 matrix1 = Matrix(matrix)
 
 print(matrix1)
-matrix1.update_matrix()
+
+inp = ""
+
+while True:
+    time.sleep(0.1)
+    matrix1.update_matrix()
+    print(matrix1)
+
+# while inp != "stop":
+#     inp = input()
+#     matrix1.update_matrix()
+#     print(matrix1)
