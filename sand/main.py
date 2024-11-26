3df0bc44b1bc1340925c4b67a69cfb6db33d0b84
nathanGouacheg22017449 <nathan.gouache@etu.univ-amu.fr>
2024-11-26 11:38:21 +0100

    Put the hourglass upside down

--- sand/main.py
+++ sand/main.py
@@ -2,14 +2,13 @@ from Matrix import *
 from Display import *
 
 matrix1 = Matrix()
-matrix1.generate_hourglass(30, chance_of_content=0.3)
+matrix1.generate_hourglass(50, chance_of_content=0.3)
 display = Display(pause_interval=0.05)
 
-has_finished = False
-
-while not has_finished:
+while True:
     display.draw_frame(matrix1.matrix_content)
-    has_finished = matrix1.update_matrix()
+    if matrix1.update_matrix():
+        matrix1.matrix_content.reverse()
 
 # Closes x seconds after the final result
 plt.pause(5)
