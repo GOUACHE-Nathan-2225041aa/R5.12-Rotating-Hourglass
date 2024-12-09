043b67cb2c4f774f2bf4e7304edcb8f50d0a4691
nathanGouacheg22017449 <nathan.gouache@etu.univ-amu.fr>
2024-12-09 17:13:34 +0100

    Still have some todos but at least everything runs and rotates now !

--- sand/Display.py
+++ sand/Display.py
@@ -1,15 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.colors import ListedColormap
+from math import cos, sin
 
 
 class Display:
     def __init__(self, pause_interval=0.00001, angle=0):
-        self.fig = plt.figure()
         self.pause_interval = pause_interval
         self.angle = angle
+        self.fig, self.ax = plt.subplots()
+        plt.ion()
 
-    def draw_frame(self, matrix=[[0],[0]]):
+    def draw_frame(self, matrix=[[0], [0]], angle=90):
+        self.ax.clear()
+
+        self.ax.set_xlim([0, len(matrix[0])])
+        self.ax.set_ylim([0, len(matrix)])
+        self.ax.set_aspect('equal')
+        self.ax.autoscale(False)
 
         matrix = np.array(matrix)
 
@@ -17,20 +24,37 @@ class Display:
         rows, cols = matrix.shape
 
         # Go through the matrix to draw the points
-        # @todo : Rotate the matrix
+        # @todo : Rotate the matrix around itself
+        # @todo : Translate everything in english
         for i in range(rows):
             for j in range(cols):
                 if matrix[i, j] == 1:
                     color = "yo"
                 elif matrix[i, j] == 2:
                     color = "ko"
-                else:
-                    color = "wo"
-                plt.plot(j*0.1, -i*0.1, color,
-                         label=f'Value: {matrix[i, j]}' if i == 0 and j == 0 else "")  # -i pour inverser les axes
+                if matrix[i, j] in [1, 2]:
+                    coordonnees_point = self.get_rotated_point([j, -i], angle)
+                    plt.plot(coordonnees_point[0], coordonnees_point[1], color,
+                             label=f'Value: {matrix[i, j]}' if i == 0 and j == 0 else "")
 
         plt.draw()
         plt.pause(self.pause_interval)
+
+    def get_rotated_point(self, point, angle):
+
+        angle = np.radians(angle)
+
+        matrice_rotation = np.matrix([
+            [cos(angle), -sin(angle), 0],
+            [sin(angle), cos(angle), 0],
+            [0, 0, 1]
+        ])
+
+        coordonnees_homogenes = np.matrix([[point[0]], [point[1]], [1]])
+
+        coordonnes_homogenes_rotated = matrice_rotation @ coordonnees_homogenes
+
+        return coordonnes_homogenes_rotated
 
     def set_angle(self, new_angle):
         self.angle = new_angle
--- sand/main.py
+++ sand/main.py
@@ -4,9 +4,12 @@ from Matrix import *
 from Display import *
 
 matrix1 = Matrix()
-matrix1.generate_hourglass(20, chance_of_content=0.2)
-display = Display(pause_interval=0)
+matrix1.generate_hourglass(10, chance_of_content=0.2)
+display = Display(pause_interval=0.01)
+
+angle = 0
 
 while True:
-    display.draw_frame(matrix1.matrix_content)
     matrix1.update_matrix()
+    display.draw_frame(matrix1.matrix_content, angle)
+    angle += 1
