b4fb33e5ebf2c2be01909b810c563d466a463549
Clément <clement.salomon@etu.univ-amu.fr>
2024-12-09 16:49:00 +0100

    Rotate matrix

--- sand/main.py
+++ sand/main.py
@@ -4,9 +4,11 @@ from Matrix import *
 from Display import *
 
 matrix1 = Matrix()
-matrix1.generate_hourglass(20, chance_of_content=0.2)
-display = Display(pause_interval=0)
+matrix1.generate_hourglass(15, chance_of_content=0.2)
+display = Display(pause_interval=0.3)
 
 while True:
     display.draw_frame(matrix1.matrix_content)
     matrix1.update_matrix()
+    print(matrix1)
+    break
